900ff72cb3def5d40d01615e3d3e65d79286d4b9
OldManUmby <b.a.umberger@me.com>
2025-01-26 22:53:33 -0600

    Updated Footer Copyright

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -1,9 +1,11 @@
+import config from "../../config.yaml"
+
 export function renderFooter() {
   const footer = document.getElementById("footer")
   if (footer) {
     footer.innerHTML = `
             <div class="container mx-auto text-center">
-                <p>&copy; 2023 Grog CMS. All rights reserved.</p>
+                <p>${config.footer.copyright}</p>
                 <p class="mt-2">
                     <a href="#" class="hover:text-gray-300">Privacy Policy</a> | 
                     <a href="#" class="hover:text-gray-300">Terms of Service</a>
